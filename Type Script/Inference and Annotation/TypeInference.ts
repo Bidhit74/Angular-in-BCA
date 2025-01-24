f82bf762271541320cdc32afbead39b4752ea8ad
Bidhit <bidhitkumar61@gmail.com>
2025-01-24 13:58:56 +0530

    Add Type Inference example in TypeScript

---
+++ Type Script/Inference and Annotation/TypeInference.ts
@@ -0,0 +1,4 @@
+//  Type Inference: TypeScript automatically determines the type based on the assigned value.
+
+let age = 25; // Type inferred as number
+let firstName = "Bidhit"; // Type inferred as string

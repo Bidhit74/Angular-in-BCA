028408ffe6c8f68662b3d8faf21d6fa1edc9740c
Bidhit <bidhitkumar61@gmail.com>
2025-01-25 15:36:35 +0530

    Add math utility functions for addition and subtraction

---
+++ Type Script/Miscellaneous/Modules/mathUtils.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.subtract = exports.add = void 0;
+// Named Exports
+var add = function (a, b) { return a + b; };
+exports.add = add;
+var subtract = function (a, b) { return a - b; };
+exports.subtract = subtract;

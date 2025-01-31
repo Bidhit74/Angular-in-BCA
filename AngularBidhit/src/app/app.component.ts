2877c04c3a7ec22679ec59b68e892e123236ec1e
Bidhit <bidhitkumar61@gmail.com>
2025-01-31 23:12:33 +0530

    Refactor app component to replace multiple components with a single data binding component

--- AngularBidhit/src/app/app.component.ts
+++ AngularBidhit/src/app/app.component.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
-import { AdminComponent } from './components/admin/admin.component';
-import { UserComponent } from './components/user/user.component';
-import { SinglePageComponent } from './components/single-page/single-page.component';
+import { DataBindingComponent } from './components/data-binding/data-binding.component';
 
 @Component({
   selector: 'app-root',
-  imports: [AdminComponent, UserComponent, SinglePageComponent],
+  imports: [DataBindingComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })

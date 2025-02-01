b136421048a657c6d661cdc2d7ce4554695de240
Bidhit <bidhitkumar61@gmail.com>
2025-02-01 20:13:16 +0530

    Add linked signal functionality to Signal component with dynamic name and email updates

--- AngularBidhit/src/app/components/signal/signal.component.ts
+++ AngularBidhit/src/app/components/signal/signal.component.ts
@@ -1,4 +1,9 @@
-import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  linkedSignal,
+  signal,
+} from '@angular/core';
 
 @Component({
   selector: 'app-signal',
@@ -19,5 +24,34 @@ export class SignalComponent {
       this.courseName = 'React Js';
       this.lastName.set('Kumar'); // signal value changed ui update
     }, 5000);
+  }
+
+  //Linked Signal in Angular - 19
+  firstName1 = signal<string>('Binit');
+  lastName1 = signal<string>('Chaudhary');
+
+  fullName = linkedSignal({
+    source: this.firstName1,
+    computation: (newValue, oldValue) => {
+      debugger;
+      const fullName = newValue + ' ' + this.lastName1();
+      return fullName;
+    },
+  });
+
+  changeName() {
+    this.firstName1.set('Navnit');
+  }
+
+  user = signal({ id: 121, name: 'Bidhit' });
+
+  email = linkedSignal({
+    source: this.user,
+    computation: (user) => `${user.name + user.id}@gmail.com`,
+    equal: (a: any, b: any) => a.id !== b.id,
+  });
+
+  changeId() {
+    this.user.set({ id: 111, name: 'Bidhit' });
   }
 }
